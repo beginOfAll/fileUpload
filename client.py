33d97095f953d57d771d821d1625ad3bb2b86b9f
beginOfAll <857599073@qq.com>
2017-07-25 22:32:37 +0800

    add download function
    version upgrade.
    from servers download file into client.

--- client.py
+++ client.py
@@ -10,48 +10,100 @@ def filePathInput():
     return path
 
 
+def folderPathInput():
+    path = input("Folder AllPath：")
+    if not (os.path.exists(path) and os.path.isdir(path)):
+        print("Path Error,Retry Pls：")
+        path = folderPathInput()
+    return path
 
 
-def serverIPInput():
+def mode_input():
+    m_mode = input("Input number : 1.(upload) or 2.(download):")
+    if m_mode.isalnum():
+        m_mode = int(m_mode)
+        if m_mode != 1 and m_mode != 2:
+            print("only 1 or 2 pls !")
+            m_mode = mode_input()
+    else:
+        print("only 1 or 2 pls !")
+        m_mode = mode_input()
+    return m_mode
+
+
+def server_ip_input():
     return input("Input Server IP：")
 
 
-def openSocket(ip):
+def open_socket(ip):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, 10000))
     print(s.recv(1024).decode('utf-8'))
     return s
 
 
-if __name__ == '__main__':
+def mode_upload(this_socket):
+    this_socket.send("upload".encode('utf-8'))
+    print(this_socket.recv(1024).decode('utf-8'))
     file_path = filePathInput()
     file_name = os.path.basename(file_path)
-    file_size = os.stat(path=file_path).st_size
+    file_size = os.stat(file_path).st_size
     send_file_info = "head|" + file_name + "|" + str(file_size)
     print("FileName：", file_name, "，FileSize：", str(file_size))
-    try:
-        this_socket = openSocket(serverIPInput())
-        this_socket.send(send_file_info.encode('utf-8'))
-        send_size = 0
-        residue_size = 0
-        Flag = True
-        f = open(file_path, 'rb')
-        while Flag:
-            residue_size = file_size - send_size
-            if residue_size < 1024:
-                data = f.read(residue_size)
-                Flag = False
+    this_socket.send(send_file_info.encode('utf-8'))
+    if this_socket.recv(1024).decode('utf-8') == 'ok':
+        flag = True
+    send_size = 0
+    f = open(file_path, 'rb')
+    while flag:
+        residue_size = file_size - send_size
+        if residue_size < 1024:
+            data = f.read(residue_size)
+            flag = False
+        else:
+            data = f.read(1024)
+            send_size += 1024
+        this_socket.send(data)
+    f.close()
+    print(this_socket.recv(1024).decode('utf-8'))
+
+
+def mode_download(this_socket):
+    this_socket.send("download".encode('utf-8'))
+    print(this_socket.recv(1024).decode('utf-8'))
+    folder = folderPathInput()
+    if not folder.endswith('\\'):
+        folder = folder + '\\'
+    check_code, file_name, file_size_s = this_socket.recv(1024).decode('utf-8').split('|')
+    file_size = int(file_size_s)
+    if check_code == "head":
+        this_socket.send("ok".encode('utf-8'))
+        recv_size = 0
+        file_path = folder + file_name
+        f = open(file_path, 'wb')
+        while True:
+            if recv_size < file_size:
+                data = this_socket.recv(1024)
+                recv_size += len(data)
             else:
-                data = f.read(1024)
-                send_size += 1024
-            this_socket.send(data)
+                break
+            f.write(data)
         f.close()
-        print(this_socket.recv(1024).decode('utf-8'))
-        this_socket.close()
+        this_socket.send("download file success".encode('utf-8'))
+        print("receive file success, file path : ", file_path)
+
+
+if __name__ == '__main__':
+    mode = mode_input()
+    one_socket = open_socket(server_ip_input())
+    try:
+        if mode == 1:
+            mode_upload(one_socket)
+        else:
+            mode_download(one_socket)
+        one_socket.close()
     except():
         print("Connecting Error，Process Closed")
-        if f != None and f.closed == False:
-            f.close()
-        if this_socket != None:
-            this_socket.close()
+        if one_socket is not None:
+            one_socket.close()
     input("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
--- server.py
+++ server.py
@@ -1,57 +1,121 @@
 import socket
 import threading
 import os
+import zipfile
 
-ip = ""
-base_path = ""
+upload_folder_path = ""
+download_folder_path = ""
 
 
-def folderPathInput():
-    path = input("Save upload file floder path:")
+def server_ip_input():
+    return input("IP：")
+
+
+def folder_path_input(message):
+    path = input("Save " + message + " file folder path:")
     if not (os.path.exists(path) and os.path.isdir(path)):
         print("Path error，Retry pls")
-        path = folderPathInput()
+        path = folder_path_input(message)
+    else:
+        if not path.endswith('/'):
+            path = path + '/'
     return path
 
 
-def tcp_link(sock, addr):
-    global ip, base_path
-    print('accept new connection from %s:%s' % addr)
-    sock.send(("welcome to connect " + ip).encode('utf-8'))
-    check_code, file_name, file_size_s = sock.recv(1024).decode('utf-8').split('|')
+def mode_upload(this_socket):
+    global upload_folder_path
+    check_code, file_name, file_size_s = this_socket.recv(1024).decode('utf-8').split('|')
     file_size = int(file_size_s)
     if check_code == "head":
+        this_socket.send("ok".encode('utf-8'))
         recv_size = 0
-        file_path = base_path + file_name
+        file_path = upload_folder_path + file_name
         f = open(file_path, 'wb')
         while True:
             if recv_size < file_size:
-                data = sock.recv(1024)
+                data = this_socket.recv(1024)
                 recv_size += len(data)
             else:
                 break
             f.write(data)
         f.close()
-        sock.send("upload file success".encode('utf-8'))
+        this_socket.send("upload file success".encode('utf-8'))
         print("upload success, file path : ", file_path)
+
+
+def zip_model(path):
+    os.chdir(path)
+    file_tree = os.walk('.')
+    zip_file = zipfile.ZipFile("../file.zip", 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in file_tree:
+        for one_file in files:
+            zip_file.write(os.path.join(root, one_file))
+        if len(files) == 0:
+            empty_dir = zipfile.ZipInfo(root + '/')
+            zip_file.writestr(empty_dir, '')
+    zip_file.close()
+    return os.path.abspath("../file.zip")
+
+
+def mode_download(this_socket):
+    global download_folder_path
+    f_list = os.listdir(download_folder_path)
+    if len(f_list) == 0:
+        pass
+    else:
+        if len(f_list) == 1 and os.path.isfile(os.path.join(download_folder_path, f_list[0])):
+            file_path = os.path.join(download_folder_path, f_list[0])
+        else:
+            file_path = zip_model(download_folder_path)
+        file_name = os.path.basename(file_path)
+        file_size = os.stat(file_path).st_size
+        send_file_info = "head|" + file_name + "|" + str(file_size)
+        this_socket.send(send_file_info.encode('utf-8'))
+        if this_socket.recv(1024).decode('utf-8') == 'ok':
+            flag = True
+        send_size = 0
+        f = open(file_path, 'rb')
+        while flag:
+            residue_size = file_size - send_size
+            if residue_size < 1024:
+                data = f.read(residue_size)
+                flag = False
+            else:
+                data = f.read(1024)
+                send_size += 1024
+            this_socket.send(data)
+        f.close()
+        print(this_socket.recv(1024).decode('utf-8'))
+
+
+def tcp_link(sock, addr, ip):
+    print('accept new connection from %s:%s' % addr)
+    sock.send(("welcome to connect " + ip).encode('utf-8'))
+
+    mode = sock.recv(1024).decode('utf-8')
+    if mode == 'upload':
+        sock.send("begin to upload ".encode('utf-8'))
+        mode_upload(sock)
+    else:
+        sock.send("begin to download ".encode('utf-8'))
+        mode_download(sock)
     sock.close()
     print('connection from %s:%s closed.' % addr)
 
 
-def serverIPInput():
-    return input("IP：")
-
-
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ip = serverIPInput()
-    base_path = folderPathInput()
-    if not base_path.endswith('\\'):
-        base_path = base_path + '\\'
-    s.bind((ip, 10000))
+    # self_ip = server_ip_input()
+    self_ip = "127.0.0.1"
+    # upload_folder_path = folder_path_input("upload")
+    upload_folder_path = "D:/temp/upload/"
+    # download_folder_path = folder_path_input("download")
+    download_folder_path = "D:/temp/download/"
+
+    s.bind((self_ip, 10000))
     s.listen(5)
     print("wait for connection")
     while True:
-        this_socket, client_address = s.accept()
-        t = threading.Thread(target=tcp_link, args=(this_socket, client_address))
+        one_socket, client_address = s.accept()
+        t = threading.Thread(target=tcp_link, args=(one_socket, client_address, self_ip))
         t.start()
